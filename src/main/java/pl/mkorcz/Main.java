6e4419978eb413167a14608a8e72f6916e009d16
Michal <mkorcz01@gmail.com>
2020-11-25 20:29:01 +0100

    second stage, What's my name?

--- src/main/java/pl/mkorcz/Main.java
+++ src/main/java/pl/mkorcz/Main.java
@@ -1,8 +1,17 @@
 package pl.mkorcz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Mr. Robot.");
-        System.out.println("I as created in 2020");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
     }
 }

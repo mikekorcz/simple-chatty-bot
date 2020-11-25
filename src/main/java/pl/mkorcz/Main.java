b6856a47b48023b21acdafba7889f6f1747df6b5
Michal <mkorcz01@gmail.com>
2020-11-25 20:25:25 +0100

    first stage, hello

--- src/main/java/pl/mkorcz/Main.java
+++ src/main/java/pl/mkorcz/Main.java
@@ -2,6 +2,7 @@ package pl.mkorcz;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello! My name is Mr. Robot.");
+        System.out.println("I as created in 2020");
     }
 }

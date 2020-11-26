0bd5b1aad69326add055b5e27deb6457714a2d32
Michal <mkorcz01@gmail.com>
2020-11-26 20:05:59 +0100

    stage three - let me quess your number!

--- src/main/java/pl/mkorcz/Main.java
+++ src/main/java/pl/mkorcz/Main.java
@@ -13,5 +13,14 @@ public class Main {
         String yourName = scanner.nextLine();
 
         System.out.println("What a great name you have, " + yourName + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+
+        int yourAge = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
     }
 }
